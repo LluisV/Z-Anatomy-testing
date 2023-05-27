e0c868e3919be42994bba64afb47436d75143664
akshitagupta15june <akshitagupta15june@gmail.com>
2023-05-27 11:13:11 +0530

    added prefab search

--- Assets/SCENES/Level Selector/UGUI/ModelScene.cs
+++ Assets/SCENES/Level Selector/UGUI/ModelScene.cs
@@ -18,32 +18,65 @@ public class ModelScene : MonoBehaviour
         // Set the text to the selected button text
         selectedText.text = selectedButton;
 
-        // Load the corresponding prefab
-        GameObject prefab = Resources.Load<GameObject>("Prefabs/" + selectedButton);
+        // Load the "skeleton" prefab
+        GameObject skeletonPrefab = Resources.Load<GameObject>("Prefabs/Skeleton");
 
-        // Instantiate the prefab in the container
-        if (prefab != null)
+        if (skeletonPrefab != null)
         {
-            // Instantiate the prefab
-            GameObject model = Instantiate(prefab, prefabContainer.transform);
+            // Search for the corresponding GameObject with the selectedButton text inside the "skeleton" prefab
+            Transform targetTransform = FindGameObjectInChildren(skeletonPrefab.transform, selectedButton);
 
-            // Set the reference to the model GameObject
-            modelObject = model;
+            if (targetTransform != null)
+            {
+                // Instantiate the corresponding GameObject in the container
+                GameObject model = Instantiate(targetTransform.gameObject, prefabContainer.transform);
 
-            // Set the parent of the model to the ModelScene GameObject
-            model.transform.SetParent(prefabContainer.transform);
+                // Set the reference to the model GameObject
+                modelObject = model;
 
-            // Scale the model to fit the prefabContainer
-            float scaleFactor = GetScaleFactor(prefabContainer, model);
-            model.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+                // Set the parent of the model to the ModelScene GameObject
+                model.transform.SetParent(prefabContainer.transform);
 
+                // Scale the model to fit the prefabContainer
+                float scaleFactor = GetScaleFactor(prefabContainer, model);
+                model.transform.localScale = new Vector3(scaleFactor, scaleFactor, scaleFactor);
+            }
+            else
+            {
+                Debug.LogError("Could not find GameObject with the text: " + selectedButton + " inside the skeleton prefab");
+            }
         }
         else
         {
-            Debug.LogError("Could not load prefab: " + selectedButton);
+            Debug.LogError("Could not load skeleton prefab");
         }
     }
 
+    // Helper method to recursively search for a GameObject with a specific text in the hierarchy
+    private Transform FindGameObjectInChildren(Transform parent, string searchText)
+    {
+        if (parent.name == searchText)
+        {
+            return parent;
+        }
+
+        for (int i = 0; i < parent.childCount; i++)
+        {
+            Transform child = parent.GetChild(i);
+            Transform result = FindGameObjectInChildren(child, searchText);
+            if (result != null)
+            {
+                return result;
+            }
+        }
+
+        return null;
+    }
+
+
+
+
+    // Helper method to get the scale factor for a given container and object
     // Helper method to get the scale factor for a given container and object
     private float GetScaleFactor(GameObject container, GameObject obj)
     {
@@ -54,7 +87,7 @@ public class ModelScene : MonoBehaviour
         Bounds containerBounds = GetBounds(container);
 
         // Calculate the scale factor
-        float scaleFactor = containerBounds.size.magnitude / objBounds.size.magnitude;
+        float scaleFactor = containerBounds.size.magnitude / Mathf.Max(objBounds.size.x, objBounds.size.y, objBounds.size.z);
 
         return scaleFactor;
     }

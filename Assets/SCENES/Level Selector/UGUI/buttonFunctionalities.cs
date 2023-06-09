d723156e65c02e20dc4be05e2b76f10c777638fd
Lluís Vinent <lluisvinentdev@gmail.com>
2023-06-09 20:12:22 +0200

    Minor fixes

--- Assets/SCENES/Level Selector/UGUI/buttonFunctionalities.cs
+++ Assets/SCENES/Level Selector/UGUI/buttonFunctionalities.cs
@@ -4,37 +4,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using TMPro;
-    public class buttonFunctionalities : MonoBehaviour
+public class buttonFunctionalities : MonoBehaviour
+{
+
+    private List<string> bodyPartList;
+    private string levelName;
+
+    public void onButtonClick(GameObject buttonGameObject)
     {
-        
-        public void onButtonClick(GameObject buttonGameObject)
+        string buttonText = buttonGameObject.GetComponentInChildren<TextMeshProUGUI>().text;
+        Debug.Log(buttonText);
+
+        // If a button was previously selected, reset its color to its original color
+        if (TestingLoadCSVData.Instance.flag == 1)
         {
-            string buttonText = buttonGameObject.GetComponentInChildren<TextMeshProUGUI>().text;
-            Debug.Log(buttonText);
-
-            // If a button was previously selected, reset its color to its original color
-            if (TestingLoadCSVData.Instance.flag == 1)
-            {
-                TestingLoadCSVData.Instance.lastSelectedButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
-            }
-
-            // Set the color of the current button to yellow
-            Button currentButton = buttonGameObject.GetComponent<Button>();
-
-            currentButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
-
-            // Store the selected button's text in PlayerPrefs
-            PlayerPrefs.SetString("SelectedButton", buttonText);
-
-            // Save a reference to the last selected button
-            TestingLoadCSVData.Instance.lastSelectedButton = currentButton;
-            TestingLoadCSVData.Instance.flag = 1;
-
+            TestingLoadCSVData.Instance.lastSelectedButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.white;
         }
 
-        public void onPlayButtonClick()
-        {
-            // Load the "modelscene" scene
-            SceneManager.LoadScene("ModelScene");
-        }
+        // Set the color of the current button to yellow
+        Button currentButton = buttonGameObject.GetComponent<Button>();
+
+        currentButton.GetComponentInChildren<TextMeshProUGUI>().color = Color.yellow;
+
+        // Convert the list to a JSON string (so we can send it as a string)
+        string jsonString = JsonUtility.ToJson(bodyPartList);
+        // Send the list
+        PlayerPrefs.SetString("BodypartList", jsonString);
+        // Send the level name
+        PlayerPrefs.SetString("LevelName", levelName);
+
+        // Save a reference to the last selected button
+        TestingLoadCSVData.Instance.lastSelectedButton = currentButton;
+        TestingLoadCSVData.Instance.flag = 1;
+
     }
+
+    public void onPlayButtonClick()
+    {
+        // Load the "modelscene" scene
+        SceneManager.LoadScene("ModelScene");
+    }
+}

7d24b2f46ead38dfce9b1e4da1f2082f6c79a46e
Lalit Kumar Sharma <lalitkumarsharma.jsr@gmail.com>
2023-06-17 16:51:12 +0530

    Added Tooltip functionality for the Cursor.

---
+++ Assets/TooltipManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class TooltipManager : MonoBehaviour
+{
+    public static TooltipManager _instance;
+
+    public TextMeshProUGUI textComponent;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = true;
+        gameObject.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Input.mousePosition;        
+    }
+
+    public void SetandShowToolTip(string message)
+    {
+        gameObject.SetActive(true);
+        textComponent.text = message;
+    }
+
+    public void HideToolTip()
+    {
+        gameObject.SetActive(false);
+        textComponent.text = string.Empty;
+    }
+}

51f1f99d009b831300960ec02e37f5471fab3387
Lalit Kumar Sharma <lalitkumarsharma.jsr@gmail.com>
2023-06-20 15:31:16 +0530

    Added a PauseMenu for the Game.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public static bool isPaused;
+    //Use this isPaused bool globaly in any script to add paused functionality
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoBack()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Lvl_Selector");
+    }
+
+}
